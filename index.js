c2229658844cf9e4138a343fe6a9bb418473742e
Nica <nicamaezurbano59@gmail.com>
2025-04-26 17:01:40 +0800

    Modify index.js (try new approach)

--- index.js
+++ index.js
@@ -29,16 +29,8 @@ let id = 0;
 
 app.post('/api/shorturl', function(req, res) {
   let url = req.body.url;
-  let urlPattern = /^https?:\/\/([a-zA-Z0-9-]+\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(:\d+)?(\/[^\s]*)?$/;
 
-  if(!urlPattern.test(url)) 
-  {
-    // Not match
-    res.json({
-      "error":"Invalid URL"
-    });
-  }
-  else
+  try 
   {
     // Retrieve hostname
     let hostname = new URL(url).hostname;
@@ -79,7 +71,64 @@ app.post('/api/shorturl', function(req, res) {
         })
       }
     });
+  } 
+  catch (e) {
+    res.json({
+      "error":"Invalid URL"
+    });
   }
+
+  // let urlPattern = /^https?:\/\/([a-zA-Z0-9-]+\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(:\d+)?(\/[^\s]*)?$/;
+
+  // if(!urlPattern.test(url)) 
+  // {
+  //   // Not match
+  //   res.json({
+  //     "error":"Invalid URL"
+  //   });
+  // }
+  // else
+  // {
+  //   // Retrieve hostname
+  //   let hostname = new URL(url).hostname;
+    
+  //   // Check the hostname if valid
+  //   dns.lookup(hostname, (err, address, family) => {
+  //     if(err)
+  //     {
+  //       console.log('error:' + err);
+  //       res.json({
+  //         "error":"Invalid Hostname"
+  //       });
+  //     }
+  //     else
+  //     {
+  //       // Check if the url is existing on the array
+  //       let isExists = false;
+  //       url_array.forEach(element => {
+  //         if(element.original_url == url)
+  //         {
+  //           isExists = true;
+  //         }
+  //       });
+
+  //       if(!isExists)
+  //       {
+  //         id++;
+  //         url_array.push({'original_url': url, 'id': id});
+  //       }
+
+  //       // Retrieve the short url based on its index +1
+  //       let shorturl = id;
+  //       console.log(url_array);
+
+  //       res.json({
+  //         'original_url': url,
+  //         'short_url': shorturl
+  //       })
+  //     }
+  //   });
+  // }
 });
 
 app.get('/api/shorturl/:shorturl', function(req, res) {

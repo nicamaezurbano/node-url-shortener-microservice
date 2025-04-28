a0388d353552267788d4926ca7a926c826bd2e28
Nica <nicamaezurbano59@gmail.com>
2025-04-28 08:49:48 +0800

    Modify index.js to follow the pattern : http://www.example.com

--- index.js
+++ index.js
@@ -30,105 +30,67 @@ let id = 0;
 app.post('/api/shorturl', function(req, res) {
   let url = req.body.url;
 
-  try 
+  // Regex to follow the pattern : http://www.example.com
+  let urlPattern = /^https?:\/\/([a-zA-Z0-9-]+\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(:\d+)?(\/[^\s]*)?$/;
+
+  if(!urlPattern.test(url)) 
   {
-    // Retrieve hostname
-    let hostname = new URL(url).hostname;
-    
-    // Check the hostname if valid
-    dns.lookup(hostname, (err, address, family) => {
-      if(err)
-      {
-        console.log('error:' + err);
-        res.json({
-          "error":"Invalid Hostname"
-        });
-      }
-      else
-      {
-        // Check if the url is existing on the array
-        let isExists = false;
-        url_array.forEach(element => {
-          if(element.original_url == url)
-          {
-            isExists = true;
-          }
-        });
-
-        if(!isExists)
-        {
-          id++;
-          url_array.push({'original_url': url, 'id': id});
-        }
-
-        // Retrieve the short url based on its index +1
-        let shorturl = id;
-        console.log(url_array);
-
-        res.json({
-          'original_url': url,
-          'short_url': shorturl
-        })
-      }
-    });
-  } 
-  catch (e) {
+    // Not match
     res.json({
       "error":"Invalid URL"
     });
   }
+  else
+  {
+    try 
+    {
+        
+      // Retrieve hostname
+      let hostname = new URL(url).hostname;
+      
+      // Check the hostname if valid
+      dns.lookup(hostname, (err, address, family) => {
+        if(err)
+        {
+          console.log('error:' + err);
+          res.json({
+            "error":"Invalid Hostname"
+          });
+        }
+        else
+        {
+          // Check if the url is existing on the array
+          let isExists = false;
+          url_array.forEach(element => {
+            if(element.original_url == url)
+            {
+              isExists = true;
+            }
+          });
 
-  // let urlPattern = /^https?:\/\/([a-zA-Z0-9-]+\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(:\d+)?(\/[^\s]*)?$/;
+          if(!isExists)
+          {
+            id++;
+            url_array.push({'original_url': url, 'id': id});
+          }
 
-  // if(!urlPattern.test(url)) 
-  // {
-  //   // Not match
-  //   res.json({
-  //     "error":"Invalid URL"
-  //   });
-  // }
-  // else
-  // {
-  //   // Retrieve hostname
-  //   let hostname = new URL(url).hostname;
-    
-  //   // Check the hostname if valid
-  //   dns.lookup(hostname, (err, address, family) => {
-  //     if(err)
-  //     {
-  //       console.log('error:' + err);
-  //       res.json({
-  //         "error":"Invalid Hostname"
-  //       });
-  //     }
-  //     else
-  //     {
-  //       // Check if the url is existing on the array
-  //       let isExists = false;
-  //       url_array.forEach(element => {
-  //         if(element.original_url == url)
-  //         {
-  //           isExists = true;
-  //         }
-  //       });
+          // Retrieve the short url based on its index +1
+          let shorturl = id;
+          console.log(url_array);
 
-  //       if(!isExists)
-  //       {
-  //         id++;
-  //         url_array.push({'original_url': url, 'id': id});
-  //       }
-
-  //       // Retrieve the short url based on its index +1
-  //       let shorturl = id;
-  //       console.log(url_array);
-
-  //       res.json({
-  //         'original_url': url,
-  //         'short_url': shorturl
-  //       })
-  //     }
-  //   });
-  // }
+          res.json({
+            'original_url': url,
+            'short_url': shorturl
+          })
+        }
+      });
+    } 
+    catch (e) {
+      res.json({
+        "error":"Invalid URL"
+      });
+    }
+  }
 });
 
 app.get('/api/shorturl/:shorturl', function(req, res) {
